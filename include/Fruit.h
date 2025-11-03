049baacdf9f673ac350b9e5dd58156b8cb89e669
xd4rk1ng <rui.santos.maia@gmail.com>
2025-11-03 02:04:00 +0000

    dev

--- include/Fruit.h
+++ include/Fruit.h
@@ -1,4 +1,4 @@
-#include "Atlas.h"
+
 
 
 
--- src/Fruit.cpp
+++ src/Fruit.cpp
@@ -1,6 +1,10 @@
 #include "Fruit.h"
-
+#include "Atlas.h"
 Fruit::Fruit()
 {
-    m_pos = Vector2{.x = 1 };
+    Vector2 m_pos = Vector2{.x = 1 , .y = 1};
 }
+uint8_t Fruit::getId() { return Atlas::FRUIT; } 
+Vector2 Fruit::getPos() { return m_pos;}
+
+void Fruit::update() {  }
